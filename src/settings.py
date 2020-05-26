407b8b5051df3e6b8955f32b4c823672cd7098c2
gt <jack139@gmail.com>
2020-05-26 23:48:16 +0800

    双算法并行识别

--- src/settings.py
+++ src/settings.py
@@ -8,6 +8,12 @@ ALGORITHM = {
     'evo'  : { 'distance_threshold' : 1.25, 'ext' : '.evo.clf', 'module' : 'face_evoLVe.verify' },
 }
 
+algorithm_settings = {
+    1 : [ 'vgg', 'data/model/train2_senet50.vgg.clf' ], # 优先返回
+    2 : [ 'evo', 'data/model/train2_5.evo.clf' ],
+    #1 : [ 'rec', 'data/model/train.rec.clf' ],
+    #2 : [ 'vgg', 'data/model/train_senet50.vgg.clf' ],
+}
 
 def import_verify(face_algorithm):
     return import_module(ALGORITHM[face_algorithm]['module'])
